6404441c1603213adff71627f3421b781d15a1e9
Rose Baqueran <lanberttecson@gmail.com>
2024-11-29 21:04:44 +0800

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') == 'production') {
+/*         if (env('APP_ENV') == 'production') {
             $this->app['request']->server->set('HTTPS', true);
-        }
+        } */
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,40 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin')
+const plugin = require('tailwindcss/plugin');
 
 export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px'
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
     extend: {
-        textShadow: {
-          sm: '0 1px 2px var(--tw-shadow-color)',
-          DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-          lg: '0 8px 16px var(--tw-shadow-color)',
-        },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
     },
   },
   plugins: [
     require('tailwindcss-animated'),
-    require('flowbite/plugin')({
-        charts: true,
-    }),
+    require('flowbite/plugin'), // Removed invalid options
     plugin(function ({ matchUtilities, theme }) {
-        matchUtilities(
-          {
-            'text-shadow': (value) => ({
-              textShadow: value,
-            }),
-          },
-          { values: theme('textShadow') }
-        );
-      }),
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      );
+    }),
   ],
-}
+};
